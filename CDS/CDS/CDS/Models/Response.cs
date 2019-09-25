383344c92a443ddde87b950ba4f834b47c0edbdb
z1ber <ron0adal@gmail.com>
2019-09-25 09:30:44 -0600

    Agregado modelo de respuesta

---
+++ CDS/CDS/CDS/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDS.Models
+{
+    public class Response
+    {
+        public bool isSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
