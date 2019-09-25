31436325e33e10683a25d59e18f8ee9f762c7a90
z1ber <ron0adal@gmail.com>
2019-09-25 09:39:13 -0600

    Agregado servicio para mensajes generico

---
+++ CDS/CDS/CDS/Services/DialogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS.Services
+{
+    public class DialogService
+    {
+        public async Task Message(string titulo, string sms)
+        {
+            await App.Current.MainPage.DisplayAlert(titulo, sms, "Aceptar");
+        }
+        public async Task<bool> Message(string titulo, string sms, string ok, string no)
+        {
+            var rs = await App.Current.MainPage.DisplayAlert(titulo, sms, ok, no);
+            if (!rs)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
