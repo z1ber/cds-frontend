899bcd36f7fb7e1eef771ea09f9c640b581a2601
Lemus29 <wilmerlemus29@gmail.com>
2019-09-25 13:37:27 -0600

    Eliminar archivos innecesarios y agregar clases a martes y jueves

--- CDS/CDS/CDS/Views/Jueves.xaml.cs
+++ CDS/CDS/CDS/Views/Jueves.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CDS.Models;
+using CDS.Services;
+using Plugin.TextToSpeech;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +12,41 @@ using Xamarin.Forms.Xaml;
 
 namespace CDS.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Jueves : ContentPage
-	{
-		public Jueves ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jueves : ContentPage
+    {
+        HorarioService horSer;
+        List<Horario> lstHorario;
+        public Jueves()
+        {
+            InitializeComponent();
+            horSer = new HorarioService();
+            cargar();
+        }
+        /*private void Listhorario_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var studdent = e.Item as Horario;
+            DisplayAlert("Horario", studdent.Nombrem + "\n" + studdent.Docente+"\n"+studdent.Hora, "Aceptar");
+            CrossTextToSpeech.Current.Speak("llevas" + studdent.Nombrem + "con" + studdent.Docente + "de" + studdent.Hora);
+        }*/
+
+        private void Listhorario_Refreshing(object sender, EventArgs e)
+        {
+            cargar();
+        }
+        async void cargar()
+        {
+            lstHorario = await horSer.GetHorariosDiaAsync("Lunes", 1);
+            horarioList.ItemsSource = lstHorario.OrderBy(item => item.horaInicio).ToList();
+
+        }
+
+        private void HorarioList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var hora = e.SelectedItem as Horario;
+            DisplayAlert("Llevas", hora.nombreMateria + "\n" + hora.docente + "\n" + hora.horaInicio + " - " + hora.horaFin, "Aceptar");
+            CrossTextToSpeech.Current.Speak("Llevas" + hora.nombreMateria + " con " + hora.docente + " de " + hora.horaInicio + " a " + hora.horaFin);
+
+        }
+    }
 }
